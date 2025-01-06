2a7e16fd47271673a7cc7062158ded15be052031
David <davidhu_94@hotmail.com>
2025-01-06 14:57:04 +0100

    Not Implemented CRUD Controller

---
+++ API/Controllers/AnswerController.cs
@@ -0,0 +1,50 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AnswerController(IMediator mediator, ILogger<UserController> logger) : ControllerBase
+    {
+        private readonly IMediator _mediator = mediator;
+        private readonly ILogger<UserController> _logger = logger;
+
+        [HttpGet]
+        [Route("GetAllAnswers")]
+        public async Task<IActionResult> GetAllAnswers()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet]
+        [Route("GetAnswerById")]
+        public async Task<IActionResult> GetAnswerById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        [Route("Add")]
+        public async Task<IActionResult> Add()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPut("{id}")]
+        [Route("Update/{id}")]
+        public async Task<IActionResult> Update()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpDelete]
+        [Route("Delete/{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}

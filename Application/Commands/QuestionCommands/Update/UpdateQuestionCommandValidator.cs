437ff98de4a8d2a2e181c11bf9151fffa718c751
Bubbelbad <victor.ivarson@hotmail.se>
2025-01-09 11:35:23 +0100

    Update endpoint for Question

---
+++ Application/Commands/QuestionCommands/Update/UpdateQuestionCommandValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+
+namespace Application.Commands.QuestionCommands.Update
+{
+    public sealed class UpdateQuestionCommandValidator : AbstractValidator<UpdateQuestionCommand>
+    {
+        public UpdateQuestionCommandValidator()
+        {
+            RuleFor(x => x.Dto.Id)
+                .NotNull().WithMessage("Id is required.")
+                .NotEmpty().WithMessage("Id is required.");
+
+            RuleFor(x => x.Dto.Text)
+                .NotNull().WithMessage("Text is required.")
+                .NotEmpty().WithMessage("Text is required.");
+
+            RuleFor(x => x.Dto.CorrectAnswerId)
+                .NotNull().WithMessage("CorrectAnswerId is required.")
+                .NotEmpty().WithMessage("CorrectAnswerId is required.");
+        }
+    }
+}

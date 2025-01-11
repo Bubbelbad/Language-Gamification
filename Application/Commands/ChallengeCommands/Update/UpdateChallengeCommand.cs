46efd329862b0c29c1b90d32e1579b8cb6ecbe58
Bubbelbad <victor.ivarson@hotmail.se>
2025-01-11 13:13:14 +0100

    Implemented Command, Handler and Validator for Challenge update endpoint

---
+++ Application/Commands/ChallengeCommands/Update/UpdateChallengeCommand.cs
@@ -0,0 +1,11 @@
+﻿using Application.Dtos.ChallengeDtos;
+using Application.Models;
+using MediatR;
+
+namespace Application.Commands.ChallengeCommands.Update
+{
+    public sealed class UpdateChallengeCommand(UpdateChallengeDto dto) : IRequest<OperationResult<GetChallengeDto>>
+    {
+        public UpdateChallengeDto Dto { get; set; } = dto; 
+    }
+}
---
+++ Application/Commands/ChallengeCommands/Update/UpdateChallengeCommandValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+
+namespace Application.Commands.ChallengeCommands.Update
+{
+    public class UpdateChallengeCommandValidator : AbstractValidator<UpdateChallengeCommand>
+    {
+        public UpdateChallengeCommandValidator()
+        {
+            RuleFor(x => x.Dto.Id)
+                .NotNull().WithMessage("Id is required.");
+
+            RuleFor(x => x.Dto.Title)
+                .NotEmpty().WithMessage("Title is required.")
+                .Length(3, 100).WithMessage("Name must be between 3 and 100 characters.");
+
+            RuleFor(x => x.Dto.Description)
+                .NotEmpty().WithMessage("Description is required.");
+        }
+    }
+}

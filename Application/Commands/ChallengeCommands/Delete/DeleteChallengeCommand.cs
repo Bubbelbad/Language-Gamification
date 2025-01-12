749de47b030dcd05591e079b88ee0b68069fd4fd
arjan <arjan@ARRU-HI>
2025-01-12 20:59:32 +0100

    Update - Delete Challenge by id

---
+++ Application/Commands/ChallengeCommands/Delete/DeleteChallengeCommand.cs
@@ -0,0 +1,10 @@
+﻿using Application.Models;
+using MediatR;
+
+namespace Application.Commands.ChallengeCommands.Delete
+{
+    public sealed class DeleteChallengeCommand(int id) : IRequest<OperationResult<bool>>
+    {
+        public int ChallengeId { get; set; } = id;
+    }
+}
---
+++ Application/Commands/ChallengeCommands/Delete/DeleteChallengeCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿using Application.Interfaces;
+using Application.Models;
+using Domain.Entities;
+using MediatR;
+
+namespace Application.Commands.ChallengeCommands.Delete
+{
+    internal sealed class DeleteChallengeCommandHandler(IGenericRepository<Challenge, int> repository) : IRequestHandler<DeleteChallengeCommand, OperationResult<bool>>
+    {
+        private readonly IGenericRepository<Challenge, int> _repository = repository;
+
+        public async Task<OperationResult<bool>> Handle(DeleteChallengeCommand request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var deleteSuccessful = await _repository.DeleteAsync(request.ChallengeId);
+                if (!deleteSuccessful)
+                {
+                    return OperationResult<bool>.Failure("Operation failed");
+                }
+                return OperationResult<bool>.Success(deleteSuccessful);
+            }
+            catch (Exception ex)
+            {
+                throw new ApplicationException("An error occurred while deleting the Challenge.", ex);
+            }
+            
+        }
+    }
+}

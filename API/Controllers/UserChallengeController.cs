a76663f9c89feb920a7dff4482c16fa51fc8f106
David <davidhu_94@hotmail.com>
2025-01-13 16:45:47 +0100

    UserChallenge Controller added.

---
+++ API/Controllers/UserChallengeController.cs
@@ -0,0 +1,58 @@
+﻿using Application.Dtos.QuestionDtos;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserChallengeController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        private readonly ILogger<QuestionController> _logger;
+
+        public UserChallengeController(IMediator mediator, ILogger<QuestionController> logger)
+        {
+            _mediator = mediator;
+            _logger = logger;
+
+        }
+
+        [HttpGet]
+        [Route("GetAll")]
+        public async Task<IActionResult> GetAllUserChallenges()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet]
+        [Route("GetById{id}")]
+        public async Task<IActionResult> GetUserChallengeById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        [Route("Create")]
+        public async Task<IActionResult> CreateUserChallenge()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPut]
+        [Route("Update")]
+        public async Task<IActionResult> UpdateUserChallenge()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpDelete]
+        [Route("Delete{id}")]
+        public async Task<IActionResult> DeleteUserChallenge(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}

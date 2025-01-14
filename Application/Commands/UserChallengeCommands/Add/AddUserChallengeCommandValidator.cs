65fed0188499af8e759963b526218dfa5667db91
David <davidhu_94@hotmail.com>
2025-01-14 09:42:49 +0100

    Added validators for Add and Update UserChallenge

--- Application/Commands/UserChallengeCommands/Add/AddUserChallengeCommandValidator.cs
+++ Application/Commands/UserChallengeCommands/Add/AddUserChallengeCommandValidator.cs
@@ -6,19 +6,19 @@ namespace Application.Commands.UserChallengeCommands.Add
     {
         public AddUserChallengeCommandValidator()
         {
-            //RuleFor(x => x.Dto.UserId)
-            //     .NotEmpty().WithMessage("UserId is required.");
+            RuleFor(x => x.Dto.UserId)
+                 .NotEmpty().WithMessage("UserId is required.");
 
-            //RuleFor(x => x.Dto.ChallengeId)
-            //    .NotEmpty().WithMessage("ChallengeId is required.");
+            RuleFor(x => x.Dto.ChallengeId)
+                .NotEmpty().WithMessage("ChallengeId is required.");
 
-            //RuleFor(x => x.Dto.Score)
-            //    .GreaterThanOrEqualTo(0).WithMessage("Score must be a non-negative number.")
-            //    .NotEmpty().WithMessage("Score is required.");
+            RuleFor(x => x.Dto.Score)
+                .GreaterThanOrEqualTo(0).WithMessage("Score must be a non-negative number.")
+                .NotEmpty().WithMessage("Score is required.");
 
-            //RuleFor(x => x.Dto.CompletedAt)
-            //    .NotNull().WithMessage("CompletedAt is required.")
-            //    .LessThanOrEqualTo(DateTime.UtcNow).WithMessage("CompletedAt cannot be a future date.");
+            RuleFor(x => x.Dto.CompletedAt)
+                .NotNull().WithMessage("CompletedAt is required.")
+                .LessThanOrEqualTo(DateTime.UtcNow).WithMessage("CompletedAt cannot be a future date.");
         }
     }
 }

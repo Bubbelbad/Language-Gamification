cd90e15843a477c4704c3d3445a643c742e66b0d
Bubbelbad <victor.ivarson@hotmail.se>
2025-01-20 12:25:07 +0100

    Added ScoreDtos

---
+++ Application/Dtos/ScoreDtos/AddScoreDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Application.Dtos.ScoreDtos
+{
+    public sealed class AddScoreDto
+    {
+        public string UserId { get; set; }
+        public int ChallengeId { get; set; }
+        public int Points { get; set; }
+        public DateTime CompletedAt { get; set; }
+    }
+}
---
+++ Application/Dtos/ScoreDtos/GetScoreDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Application.Dtos.ScoreDtos
+{
+    public sealed class GetScoreDto
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        public int ChallengeId { get; set; }
+        public int Points { get; set; }
+        public DateTime CompletedAt { get; set; }
+    }
+}

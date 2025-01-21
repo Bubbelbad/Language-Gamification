3a5a53fea2f0d66fa0d4a1d81eab96ca86d17a8a
Bubbelbad <victor.ivarson@hotmail.se>
2025-01-21 11:12:31 +0100

    Updated OnModelCreation and made new migrations

---
+++ Infrastructure/Migrations/20250121093802_AnotherUserChallengeMigration.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AnotherUserChallengeMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Score",
+                table: "UserChallenge",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddColumn<int>(
+                name: "CurrentQuestionIndex",
+                table: "UserChallenge",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CurrentQuestionIndex",
+                table: "UserChallenge");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Score",
+                table: "UserChallenge",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+        }
+    }
+}

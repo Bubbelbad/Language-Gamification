405f52ebabc838ebcba45dcab66bf4752da4ab19
Bubbelbad <victor.ivarson@hotmail.se>
2025-01-21 11:12:07 +0100

    Updated Entities to have same naming convention in properties

--- Application/Dtos/UserChallengeDtos/GetUserChallengeDto.cs
+++ Application/Dtos/UserChallengeDtos/GetUserChallengeDto.cs
@@ -5,7 +5,8 @@
         public int Id { get; set; }
         public string UserId { get; set; }
         public int ChallengeId { get; set; }
-        public int Score { get; set; }
-        public DateTime CompletedAt { get; set; }
+        public int CurrentQuestionIndex { get; set; }
+        public int? Score { get; set; }
+        public DateTime? CompletedAt { get; set; }
     }
 }

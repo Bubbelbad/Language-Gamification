713efc278caf0820546ea7dd081a3e8e4333bed5
Bubbelbad <victor.ivarson@hotmail.se>
2025-01-21 18:20:23 +0100

    Added new Dtos for GetNextQuestionQuery

---
+++ Application/Dtos/QuestionDtos/GetQuestionWithAnswersDto.cs
@@ -0,0 +1,12 @@
+﻿using Application.Dtos.AnswerDtos;
+using Domain.Entities;
+
+namespace Application.Dtos.QuestionDtos
+{
+    public class GetQuestionWithAnswersDto
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public List<GetSimpleAnswerDto> Answers { get; set; }
+    }
+}

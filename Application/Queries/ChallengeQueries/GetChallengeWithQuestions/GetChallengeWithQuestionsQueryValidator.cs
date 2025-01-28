5b9e869944912083d4d48b322805a0b37aeb9f73
Bubbelbad <victor.ivarson@hotmail.se>
2025-01-28 09:15:06 +0100

    Implemented validator for GetChallengeWithQuestions endpoint

---
+++ Application/Queries/ChallengeQueries/GetChallengeWithQuestions/GetChallengeWithQuestionsQueryValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace Application.Queries.ChallengeQueries.GetChallengeWithQuestions
+{
+    internal class GetChallengeWithQuestionsQueryValidator : AbstractValidator<GetChallengeWithQuestionsQuery>
+    {
+        public GetChallengeWithQuestionsQueryValidator()
+        {
+            RuleFor(x => x.ChallengeId)
+                .GreaterThan(0)
+                .WithMessage("Id must be of int greater than 0");
+        }
+    }
+}

e89844bd3f55b64f01a665dd3e85e7ae8e4f1bfa
Bubbelbad <victor.ivarson@hotmail.se>
2025-02-25 18:27:58 +0100

    Add GetAnswersByQuestionId endpoint

---
+++ Application/Queries/AnswerQueries/GetAnswersByQuestionId/GetAnswersByQuestionIdQueryValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace Application.Queries.AnswerQueries.GetAnswersByQuestionId
+{
+    internal class GetAnswersByQuestionIdQueryValidator : AbstractValidator<GetAnswersByQuestionIdQuery>
+    {
+        public GetAnswersByQuestionIdQueryValidator()
+        {
+            RuleFor(x => x.QuestionId)
+                .GreaterThan(0)
+                .WithMessage("Id must be of int greater than 0");
+        }
+    }
+}
